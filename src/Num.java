b5e6abc8722e9c1473cfdf80893ffbd53c96f9e2
Michael George <georgem@wlwv.k12.or.us>
2020-01-08 09:29:16 -0800

    added Num, ParamterPassing, and ParameterTester

---
+++ src/Num.java
@@ -0,0 +1,27 @@
+public class Num {
+    private int value;
+
+    public Num(){
+        value = 0;
+    }
+
+    public Num(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String toString(){
+        return "" + value;
+    }
+
+    public boolean equals(Num otherNum){
+        return this.value == otherNum.value;
+    }
+}
